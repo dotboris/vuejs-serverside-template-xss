1e598adafa5b7cec06ca708d5db2c10927b68cc0
Boris Bera <bera.boris@gmail.com>
2018-02-19 19:30:08 -0500

    Add vuejs app that's injectable even tho we escape

--- index.php
+++ index.php
@@ -4,7 +4,46 @@
   <title>VueJS serverside template xss</title>
 </head>
 <body>
-  hello world
-  <?= 2 + 2 ?>
+  <form action="">
+    <label>
+      <strong>Inject Here:</strong>
+      <input type="text" name="injectme"/>
+      <button>Go!</button>
+    </label>
+  </form>
+
+
+  <div id="injectable-app">
+    <div>
+      You have injected:
+      <?= htmlspecialchars($_GET['injectme']) ?>
+    </div>
+
+    <button type="button" @click="dec">-</button>
+    {{counter}}
+    <button type="button" @click="inc">+</button>
+  </div>
+
+  <script>
+    window.addEventListener('load', function () {
+      new Vue({
+        el: '#injectable-app',
+        data: {
+          counter: 0
+        },
+        methods: {
+          inc: function () {
+            ++this.counter;
+          },
+
+          dec: function () {
+            --this.counter;
+          }
+        }
+      });
+    });
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/vue@2.5.13/dist/vue.js"></script>
 </body>
 </html>
